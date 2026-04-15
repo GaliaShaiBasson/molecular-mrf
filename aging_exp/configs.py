0bd525936076030c3996253548eba21d8982325c
galiashaibasson <galiashaibasson@gmail.com>
2026-04-15 13:55:37 +0300

    change notebook to use generated brain mask and MRI02 image as the background image (instead of "t2w_high_mouse" - doesn't exist)
    configs - remove unused ConfigIohexol

--- aging_exp/configs.py
+++ aging_exp/configs.py
@@ -4,50 +4,6 @@ class Config:
     def get_config(self):
         return self.cfg
 
-class ConfigIohexol(Config):
-    def __init__(self):
-        config = {}
-        config['yaml_fn'] = 'scenario_io.yaml'
-        config['seq_fn'] = 'acq_protocol_io.seq'
-        config['dict_fn'] = 'dict_io.mat'
-
-        # Water_pool
-        config['water_pool'] = {}
-        config['water_pool']['t1'] = np.arange(2000, 3600+100,100) / 1000
-        config['water_pool']['t1'] = config['water_pool']['t1'].tolist()
-        config['water_pool']['t2'] = np.arange(200, 900+50, 50) / 1000
-        config['water_pool']['t2'] = config['water_pool']['t2'].tolist()
-        config['water_pool']['f'] = 1
-
-        # Solute pool  
-        config['cest_pool'] = {}
-        config['cest_pool']['Iohexol'] = {}
-        config['cest_pool']['Iohexol']['protons'] = 2
-        config['cest_pool']['Iohexol']['t1'] = [2200 / 1000]
-        config['cest_pool']['Iohexol']['t2'] = [40 / 1000]
-        config['cest_pool']['Iohexol']['k'] = np.arange(50, 1800+10, 10).tolist()        
-        config['cest_pool']['Iohexol']['f'] = np.arange(10, 100+5, 5) * config['cest_pool']['Iohexol']['protons'] / 110e3
-        config['cest_pool']['Iohexol']['f'] = config['cest_pool']['Iohexol']['f'].tolist()
-        config['cest_pool']['Iohexol']['dw'] = 4.3
-
-        # Fill initial magnetization info
-        config['scale'] = 1
-        config['reset_init_mag'] = 0
-
-        # Fill scanner info
-        config['b0'] = 4.7
-        config['gamma'] = 267.5153
-        config['b0_inhom'] = 0
-        config['rel_b1'] = 1
-
-        # Fill additional info
-        config['verbose'] = 0
-        config['max_pulse_samples'] = 100
-        config['num_workers'] = 18
-
-        self.cfg = config
-
-
 class ConfigMouse(Config):
     def __init__(self):
         config = {}
